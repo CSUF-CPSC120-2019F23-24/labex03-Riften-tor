807648be4b8c0f2220b9e78ffc1476fb6f6ad593
riften <riften@cs104u-5d.acad.fullerton.edu>
2019-09-07 14:13:01 -0700

    created a program that calculates a person's height

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,20 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+  {
+    int height, feet, inches;
+
+    std::cout << "Please enter the person's height in inches: ";
+    std::cin >> height;
+
+    //Calculates person's height
+    feet = height / 12;
+    inches = height % 12;
+
+    //Prints height
+    std::cout << "That person is " << feet << "'" << inches << '"' << std::endl;
+
+    return 0;
+  }
